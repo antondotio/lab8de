027f5e453b55b2ac1e590716cadb2a0f840c1036
Anton Santos <43733827+antondotio@users.noreply.github.com>
2018-11-21 23:24:46 -0700

    8e

--- hydro.h
+++ hydro.h
@@ -7,11 +7,11 @@ void displayHeader();
 int readData(FlowList &list);
 int menu();
 void display(FlowList &list, int numrecords);
-void addData();
-void removeData();
-double average(FlowList &list);
-double median(FlowList &list);
-void saveData();
+void addData(FlowList &list, int *numrecords);
+void removeData(FlowList &list, int *numrecords);
+double average(FlowList &list, int numrecords);
+double median(FlowList &list, int numrecords);
+void saveData(FlowList &list);
 void pressEnter();
 
 #endif
